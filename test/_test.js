830cf8b436f508573c6b5786862f495d0d9a8f2f
Alyson Takai <shtakai@gmail.com>
2016-07-07 14:35:49 -0700

    added

---
+++ test/_test.js
@@ -0,0 +1,14 @@
+var _ = require('underscore');
+var assert = require('power-assert');
+var expect = require('chai').expect;
+
+//var mathlib = require('../lib/mathlib.js');
+describe('lib', function(){
+
+  before(function(){
+  })
+  it('testname', function(){
+    expect(something).to.be.equal(5);
+  });
+
+});
